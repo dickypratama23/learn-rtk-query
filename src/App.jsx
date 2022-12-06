e4dd619c20bc8f0696cc1d3f18c9b8e414f29e6a
Dicky Pratama <dickypratama4792@gmail.com>
2022-12-06 14:23:45 +0700

    search book

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,68 @@
-import './App.css'
-import { useFetchBooksQuery } from './features/books/bookApiSlice.jsx'
+import { useFetchBooksQuery, useSearchBooksQuery } from './features/books/bookApiSlice.jsx'
+import { useState } from 'react'
 
 function App() {
-  const {data} = useFetchBooksQuery()
+  const [search, setSearch] = useState('php');
+  const onHandleChange = (e) => setSearch(e.target.value)
+
+  const [filter, setFilter] = useState(search);
+  const {data} = useSearchBooksQuery(filter)
+
+  const onClick = () => {
+    setFilter(search)
+    setSearch("")
+  }
 
   return (
-    <div className="App">
-      <div className="overflow-x-auto relative">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="py-3 px-6">Image</th>
-            <th scope="col" className="py-3 px-6">Title</th>
-            <th scope="col" className="py-3 px-6">ISBN</th>
-            <th scope="col" className="py-3 px-6">Price</th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            data?.books.map((book) => (
-              <tr key={book.isbn13} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  <img src={book.image} alt={book.title} width={250} />
-                </th>
-                <td className="py-4 px-6 text-xl font-bold">
-                  {book.title} <br/>
-                  <span className="text-xs">{book.subtitle}</span>
-                </td>
-                <td className="py-4 px-6">
-                  {book.isbn13}
-                </td>
-                <td className="py-4 px-6">
-                  {book.price}
-                </td>
-              </tr>
-            ))
-          }
-          </tbody>
-        </table>
+    <div className="container mx-auto w-screen px-4">
+      <div className="flex gap-2 justify-end py-3">
+        <input
+          type="text"
+          id="first_name"
+          className="border-2 text-sm rounded-md p-2.5"
+          placeholder="Search Books"
+          onChange={onHandleChange}
+        />
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2"
+          onClick={onClick}
+        >
+          Search
+        </button>
       </div>
+
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" className="py-3 px-6">Image</th>
+          <th scope="col" className="py-3 px-6">Title</th>
+          <th scope="col" className="py-3 px-6">ISBN</th>
+          <th scope="col" className="py-3 px-6">Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          data?.books.map((book) => (
+            <tr key={book.isbn13} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <img src={book.image} alt={book.title} width={250}/>
+              </th>
+              <td className="py-4 px-6 text-xl font-bold">
+                {book.title} <br/>
+                <span className="text-xs font-thin">{book.subtitle}</span>
+              </td>
+              <td className="py-4 px-6">
+                {book.isbn13}
+              </td>
+              <td className="py-4 px-6">
+                {book.price}
+              </td>
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/features/books/bookApiSlice.jsx
+++ src/features/books/bookApiSlice.jsx
@@ -3,17 +3,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const bookApiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.itbook.store/1.0',
+    baseUrl: 'https://api.itbook.store/1.0'
   }),
-  endpoints(builder) {
-    return {
-      fetchBooks: builder.query({
-        query() {
-          return `/new`;
-        }
-      })
-    }
-  }
+  endpoints: (build) => ({
+    fetchBooks: build.query({
+      query: () => `new`,
+    }),
+    searchBooks: build.query({
+      query: (search = 'php') => `search/${search}`
+    })
+  })
 })
 
-export const { useFetchBooksQuery } = bookApiSlice
+export const {useFetchBooksQuery, useSearchBooksQuery} = bookApiSlice
